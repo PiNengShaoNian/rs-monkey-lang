a70e887385a5ebc1ec1d649b223b39833941da56
quguiyou <quguiyou1076@qq.com>
2024-09-13 22:44:29 +0800

    feat: Add function evaluation

--- src/evaluator/object.rs
+++ src/evaluator/object.rs
@@ -1,9 +1,16 @@
+use crate::ast::*;
+use std::cell::RefCell;
 use std::fmt;
+use std::rc::Rc;
+use std::vec::Vec;
+
+use super::Env;
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Object {
     Int(i64),
     Bool(bool),
+    Func(Vec<Ident>, BlockStmt, Rc<RefCell<Env>>),
     Null,
     ReturnValue(Box<Object>),
     Error(String),
@@ -14,6 +21,7 @@ impl fmt::Display for Object {
         match *self {
             Object::Int(ref value) => write!(f, "{}", value),
             Object::Bool(ref value) => write!(f, "{}", value),
+            Object::Func(_, _, _) => write!(f, "fn()"),
             Object::Null => write!(f, "null"),
             Object::ReturnValue(ref value) => write!(f, "{}", value),
             Object::Error(ref value) => write!(f, "{}", value),
